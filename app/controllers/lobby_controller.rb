91303551cda39fae8672e723d91519c28898e1d7
Bailey Leuth <bailey-leuth@uiowa.edu>
2020-11-15 22:31:24 -0600

    Fixed the user_id retrival from db Co-authored-by: Mathew Seng <mathew-seng@uiowa.edu> Co-authored-by: Ram Sajja <ramteja-sajja@uiowa.edu> Co-authored-by: Dennis Kurtz <dennis-kurtz@uiowa.edu>

--- app/controllers/lobby_controller.rb
+++ app/controllers/lobby_controller.rb
@@ -1,11 +1,18 @@
 class LobbyController < ApplicationController
 
   def create
+    user_id = @current_user.select(:id).first.attributes.values[0]
     @game_id = SecureRandom.random_number(10000)
     @deck = Deck.create!({:cards => "1"})
     @discard = Deck.create!({:cards => "2"})
-    @hand = Hand.create!({:user_id => @current_user.select(:id), :cards => "3"})
-    Cardgame.create!({ :game_id => @game_id, :user_ids => @current_user.select(:id).to_s + ',',
+    @hand = Hand.create!({:user_id => user_id, :cards => "3"})
+    # puts %Q{Game ID: #{@game_id}}
+    # puts %Q{User ID: #{user_id}}
+    # puts %Q{Deck ID: #{@deck.id}}
+    # puts %Q{Discard ID: #{@discard.id}}
+    # puts %Q{Hand ID: #{@hand.id}}
+
+    Cardgame.create!({ :game_id => @game_id, :user_ids => user_id.to_s + ',',
                        :deck_ids => @deck.id.to_s + ',', :discard_ids => @discard.id.to_s + ',',
                        :hand_ids => @hand.id.to_s + ',', :started => false })
     redirect_to lobby_path(@game_id)
