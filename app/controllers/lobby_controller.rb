f145154681fec8ce5e779d09b6e4f99a1e3a19db
Bailey Leuth <bailey-leuth@uiowa.edu>
2020-11-16 00:18:54 -0600

    Finished base join game functionality. Error checking still needs to be added Co-authored-by: Ram Sajja <ramteja-sajja@uiowa.edu> Co-authored-by: Dennis Kurtz <dennis-kurtz@uiowa.edu>

--- app/controllers/lobby_controller.rb
+++ app/controllers/lobby_controller.rb
@@ -1,15 +1,25 @@
 class LobbyController < ApplicationController
 
   def create
-    user_id = @current_user.select(:id).first.attributes.values[0]
+    # TODO: Implement fixed game code length
+    # https://stackoverflow.com/questions/44031239/generating-random-number-of-length-6-with-securerandom-in-ruby
     game_id = SecureRandom.random_number(10000)
+
+    # Verifies unique game id
+    while Cardgame.exists?(game_id: game_id)
+      game_id = SecureRandom.random_number(10000)
+    end
+
+    user_id = @current_user.select(:id).first.attributes.values[0]  # Gets the user id from current user
     deck = Deck.create!({:cards => "1"})
     discard = Deck.create!({:cards => "2"})
     hand = Hand.create!({:user_id => user_id, :cards => "3"})
 
-    Cardgame.create!({ :game_id => game_id, :user_ids => [user_id, 123],
+    # TODO: Update deck_ids and discard_ids to take a serializable array
+    Cardgame.create!({ :game_id => game_id, :user_ids => [user_id],
                            :deck_ids => deck.id.to_s + ',', :discard_ids => discard.id.to_s + ',',
-                           :hand_ids => hand.id.to_s + ',', :started => false })
+                           :hand_ids => [hand.id], :started => false })
+
     redirect_to lobby_path(game_id)
   end
 
@@ -19,12 +29,18 @@ class LobbyController < ApplicationController
     if Cardgame.exists?(game_id: game_id)
 
       # Add user to game table
+      # TODO: Check to see if user is already in a game, if so delete that entry
       old_users = Cardgame.user_ids(game_id)
       Cardgame.where(game_id: game_id).update_all(user_ids: old_users.append(user_id))
 
       # Add new hand to the database
-      # TODO: finish
+      # TODO: Check to see if user currently has a hand, if so delete that entry
+      hand = Hand.create!({:user_id => user_id, :cards => "3"})
+      old_hands = Cardgame.hand_ids(game_id)
+      Cardgame.where(game_id: game_id).update_all(hand_ids: old_hands.append(hand.id))
+
       redirect_to lobby_path(game_id)
+
     else
       flash[:notice] = 'Game does not exist'
       redirect_to games_path
--- app/models/cardgame.rb
+++ app/models/cardgame.rb
@@ -1,9 +1,18 @@
 class Cardgame < ActiveRecord::Base
   serialize :user_ids, JSON
+  serialize :hand_ids, JSON
+  # TODO: uncomment when cardgame.rb has been updated
+  # serialize :deck_ids, JSON
+  # serialize :discard_ids, JSON
 
   # Returns user ids in a given game as array
   def self.user_ids(game_id)
     Cardgame.where(game_id: game_id).first[:user_ids]
+  end
+
+  # Returns hand ids in a given game as array
+  def self.hand_ids(game_id)
+    Cardgame.where(game_id: game_id).first[:hand_ids]
   end
 end
 
