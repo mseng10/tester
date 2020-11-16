d14ac6a1918d9668a06ad37a5bf6c5f9a085f981
Andrew Stjernberg <andrewstjernberg@yahoo.com>
2020-11-15 18:08:35 -0600

    started join game functionality with the database Co-authored-by: Ram <ramteja-sajja@uiowa.edu> Co-authored-by: Dennis <dennis-kurtz@uiowa.edu> Co-authored-by: Bailey <bailey-leuth@uiowa.edu>

--- app/controllers/lobby_controller.rb
+++ app/controllers/lobby_controller.rb
@@ -2,6 +2,12 @@ class LobbyController < ApplicationController
 
   def create
     @game_id = SecureRandom.random_number(10000)
+    @deck = Deck.create!({:cards => "1"})
+    @discard = Deck.create!({:cards => "2"})
+    @hand = Hand.create!({:user_id => @current_user.select(:id), :cards => "3"})
+    Cardgame.create!({ :game_id => @game_id, :user_ids => @current_user.select(:id).to_s + ',',
+                       :deck_ids => @deck.id.to_s + ',', :discard_ids => @discard.id.to_s + ',',
+                       :hand_ids => @hand.id.to_s + ',', :started => false })
     redirect_to lobby_path(@game_id)
   end
 
@@ -9,9 +15,5 @@ class LobbyController < ApplicationController
     @game_id = params[:game][:game_id]
     redirect_to lobby_path(@game_id)
   end
-
-  #def show
-  #  redirect_to lobby_path(@game_id)
-  #end
 
 end
