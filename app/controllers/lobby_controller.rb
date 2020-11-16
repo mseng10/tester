6cc907939befd6ef6e70b26c52db47d50d944f2b
Bailey Leuth <bailey-leuth@uiowa.edu>
2020-11-15 23:49:20 -0600

    Started base join game logic Co-authored-by: Dennis Kurtz <dennis-kurtz@uiowa.edu> Co-authored-by: Ram Sajja <ram-sajja@uiowa.edu>

--- app/controllers/lobby_controller.rb
+++ app/controllers/lobby_controller.rb
@@ -2,25 +2,33 @@ class LobbyController < ApplicationController
 
   def create
     user_id = @current_user.select(:id).first.attributes.values[0]
-    @game_id = SecureRandom.random_number(10000)
-    @deck = Deck.create!({:cards => "1"})
-    @discard = Deck.create!({:cards => "2"})
-    @hand = Hand.create!({:user_id => user_id, :cards => "3"})
-    # puts %Q{Game ID: #{@game_id}}
-    # puts %Q{User ID: #{user_id}}
-    # puts %Q{Deck ID: #{@deck.id}}
-    # puts %Q{Discard ID: #{@discard.id}}
-    # puts %Q{Hand ID: #{@hand.id}}
+    game_id = SecureRandom.random_number(10000)
+    deck = Deck.create!({:cards => "1"})
+    discard = Deck.create!({:cards => "2"})
+    hand = Hand.create!({:user_id => user_id, :cards => "3"})
 
-    Cardgame.create!({ :game_id => @game_id, :user_ids => user_id.to_s + ',',
-                       :deck_ids => @deck.id.to_s + ',', :discard_ids => @discard.id.to_s + ',',
-                       :hand_ids => @hand.id.to_s + ',', :started => false })
-    redirect_to lobby_path(@game_id)
+    Cardgame.create!({ :game_id => game_id, :user_ids => [user_id, 123],
+                           :deck_ids => deck.id.to_s + ',', :discard_ids => discard.id.to_s + ',',
+                           :hand_ids => hand.id.to_s + ',', :started => false })
+    redirect_to lobby_path(game_id)
   end
 
   def join
-    @game_id = params[:game][:game_id]
-    redirect_to lobby_path(@game_id)
+    game_id = params[:game][:game_id]
+    user_id = @current_user.select(:id).first.attributes.values[0]
+    if Cardgame.exists?(game_id: game_id)
+
+      # Add user to game table
+      old_users = Cardgame.user_ids(game_id)
+      Cardgame.where(game_id: game_id).update_all(user_ids: old_users.append(user_id))
+
+      # Add new hand to the database
+      # TODO: finish
+      redirect_to lobby_path(game_id)
+    else
+      flash[:notice] = 'Game does not exist'
+      redirect_to games_path
+    end
   end
 
 end
--- app/models/cardgame.rb
+++ app/models/cardgame.rb
@@ -1,4 +1,9 @@
 class Cardgame < ActiveRecord::Base
+  serialize :user_ids, JSON
 
+  # Returns user ids in a given game as array
+  def self.user_ids(game_id)
+    Cardgame.where(game_id: game_id).first[:user_ids]
+  end
 end
 
