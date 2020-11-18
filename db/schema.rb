fb0b6ce59e9c172532b4f4066fab922500137a25
Mathew Seng <mathewseng@gmail.com>
2020-11-17 21:59:52 -0600

    Add email column to users database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201117032652) do
+ActiveRecord::Schema.define(version: 20201118035821) do
 
   create_table "cardgames", force: :cascade do |t|
     t.integer "game_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20201117032652) do
     t.string  "password"
     t.string  "session_token"
     t.integer "current_game"
+    t.string  "email"
   end
 
 end
