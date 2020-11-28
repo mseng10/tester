0701353783b31139b27a5a6b5480d2c1683172cd
Mathew Seng <mathewseng@gmail.com>
2020-11-27 20:42:43 -0600

    Allow user to have decks pre-shuffled. Originally, we pass our decks in an assigned order. However, if we elect the user to have a specific number of hands, it would not make sense to have them shuffled. Allowing the user to have the decks pre-shuffled, will solve this problem.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,13 +1,17 @@
 class Deck < ActiveRecord::Base
 
-  def Deck::create_decks(number)
+  def Deck::create_decks(number,shuffled)
     deck_ids = []
     for deck in 1..number.to_i
       cards = []
       for card in 1..59 do
         cards.append(card.to_s)
       end
-      curr_deck = create!({:cards => cards})
+      if shuffled.to_s == "on"
+        curr_deck = create!({:cards => cards.shuffle})
+      else
+        curr_deck = create!({:cards => cards})
+      end
       deck_ids.append(curr_deck.id)
     end
     return deck_ids
