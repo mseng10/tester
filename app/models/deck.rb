7cccdede692df80ff8dd0ef1bd19a7013a54efdc
Mathew Seng <mathewseng@gmail.com>
2020-11-29 18:59:40 -0600

    Fix incorrect cards in Deck and Sink objects Originally, we were storing arrays in when we created the Deck and Sinks. However, we want it to be a comma separated string. This will take the array generated and join it with a comma. Finally it will store it in the objects.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -12,9 +12,9 @@ class Deck < ActiveRecord::Base
         cards.append(card.to_s)
       end
       if shuffled.to_s == "on"
-        curr_deck = create!({:cards => cards.shuffle})
+        curr_deck = create!({:cards => cards.shuffle.join(',')})
       else
-        curr_deck = create!({:cards => cards})
+        curr_deck = create!({:cards => cards.join(',')})
       end
       deck_ids.append(curr_deck.id)
     end
@@ -24,7 +24,7 @@ class Deck < ActiveRecord::Base
   def Deck::create_sinks(number)
     deck_ids = []
     for deck in 1..number.to_i
-      curr_deck = create!({:cards => []})
+      curr_deck = create!({:cards => ""})
       deck_ids.append(curr_deck.id)
     end
     return deck_ids
