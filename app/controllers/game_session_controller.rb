d71b63d8af02307362b88c475642e88c8ab7c575
Ram <ramteja-sajja@uiowa.edu>
2020-12-01 20:59:31 -0600

    Created a new class as helper to access the paramters in the api call. This will make future api calls easier to get the data from.
    Co-authored-by: Bailey Leuth <bailey-leuth@uiowa.edu>
    Co-authored-by: Mathew Seng <mathew-seng@uiowa.edu>

--- app/controllers/game_session_controller.rb
+++ app/controllers/game_session_controller.rb
@@ -127,9 +127,26 @@ class GameSessionController < ApplicationController
     # TODO: logic to move card from one user to the other
     # Need to fix the card from unicode to id
     # Location 0 is the function. each one after that is the params
-    data = request.original_url.split(/([^.]+$)/)[1].split('&')
+    apiHelper = ApiHelper.new(request.original_url)
+
     game_id = @current_user.select(:current_game).first.attributes.values[1]
     redirect_to game_session_path(game_id)
   end
-
 end
+
+class ApiHelper
+  attr_reader :function,:parameters
+  def initialize(url)
+    data = url.split(/([^.]+$)/)[1].split('&')
+    @function = data[0].split('=')[1]
+    @parameters = {}
+    data.each_with_index do |elem, i|
+      if i != 0
+        info = elem.split('=')
+        @parameters[info[0]] = info[1]
+      end
+    end
+  end
+end
+
+
