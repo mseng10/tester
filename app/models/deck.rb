723baed2a9964fec0cc22d13dbb3500c7b1aa613
Ram <ramteja-sajja@uiowa.edu>
2020-12-01 22:07:05 -0600

    made cards column in decks serilizable and cards column in hands serilizable. This wiull be helpful since now the query to the database is going to give us an array; which would be easier to use in the backend
    Co-authored-by: Bailey Leuth <bailey-leuth@uiowa.edu>
    Co-authored-by: Mathew Seng <mathew-seng@uiowa.edu>

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,8 @@
 class Deck < ActiveRecord::Base
 
+  serialize :cards, JSON
+
+
   def Deck::create_decks(number,shuffled,jokers)
     deck_ids = []
     final_value = 52
@@ -9,12 +12,12 @@ class Deck < ActiveRecord::Base
     for deck in 1..number.to_i
       cards = []
       for card in 1..final_value do
-        cards.append(card.to_s)
+        cards.append(card)
       end
       if shuffled.to_s == "on"
-        curr_deck = create!({:cards => cards.shuffle.join(',')})
+        curr_deck = create!({:cards => cards.shuffle})
       else
-        curr_deck = create!({:cards => cards.join(',')})
+        curr_deck = create!({:cards => cards})
       end
       deck_ids.append(curr_deck.id)
     end
@@ -24,10 +27,15 @@ class Deck < ActiveRecord::Base
   def Deck::create_sinks(number)
     deck_ids = []
     for deck in 1..number.to_i
-      curr_deck = create!({:cards => ""})
+      curr_deck = create!({:cards => []})
       deck_ids.append(curr_deck.id)
     end
     return deck_ids
   end
+end
+
+
+def self.cards(id)
+  Deck.where(id: id).first[:cards]
 end
 
