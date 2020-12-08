63307e692977094cfa8f0129a0ccd704e68575b3
Mathew Seng <mathew-seng@uiowa.edu>
2020-12-08 15:58:53 -0600

    Add column for flip tracking in Hand database
    This will default to all the hand flip values being true
    
    Co-authored-by: Ram Sajja <ramteja-sajja@uiowa.edu>
    Co-authored-by: Bailey Leuth <bailey-leuth@uiowa.edu>

--- app/models/hand.rb
+++ app/models/hand.rb
@@ -1,27 +1,34 @@
 class Hand < ActiveRecord::Base
 
   serialize :cards, JSON
+  serialize :user_cards_shown, JSON
 
   def Hand::create_hand(number,deck_id,user_id)
     cards = Deck.where(id: deck_id).first[:cards]
     hand_id = []
     hands_cards = []
+    hands_cards_flips = []
     for card in 1..number do
       if cards.length > 0
         card = cards.pop
         hands_cards.append(card)
+        hands_cards_flips.append(false)
       else
         flash[:notice] = 'Cannot create a hand of set size because the deck is out of cards'
       end
     end
     Deck.where(id: deck_id).update_all(cards: cards)
-    hand = create!({:user_id => user_id, :cards => hands_cards})
+    hand = create!({:user_id => user_id, :cards => hands_cards, :user_cards_shown => hands_cards_flips})
     hand_id.append(hand.id)
     return hand_id
   end
 
   def self.cards(id)
     Hand.where(id: id).first[:cards]
+  end
+
+  def self.user_cards_shown(id)
+    Hand.where(id: id).first[:user_cards_shown]
   end
 
 end
