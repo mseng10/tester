14049471f0504c733d06c2d7cd249f891fe8c4be
rsajja79 <ramteja-sajja@uiowa.edu>
2020-12-08 14:38:47 -0600

    Added switch visible javascript method to change which div is shown
    Co-authored-by: Bailey <bailey-leuth@uiowa.edu>
    Co-authored-by: Mathew <mathew-seng@uiowa.edu>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -140,3 +140,16 @@ function check_username() {
     }
 }
 
+function switchVisible() {
+    if (document.getElementById('Div1')) {
+
+        if (document.getElementById('Div1').style.display == 'none') {
+            document.getElementById('Div1').style.display = 'block';
+            document.getElementById('Div2').style.display = 'none';
+        }
+        else {
+            document.getElementById('Div1').style.display = 'none';
+            document.getElementById('Div2').style.display = 'block';
+        }
+    }
+}
