277fb60b2f978a4a601fe4cee67e50e22fa67897
Mathew Seng <mathew-seng@uiowa.edu>
2020-12-11 01:23:18 -0600

    Notify other users when a user has left or started the game.
    Originally, the user would have to restart the page if the game was to be started or ended.
    
    Now when a single user starts a game, the game will be marked as started and Pusher will reload the page. When the page notices its started it will redirect accordingly.
    
    The same goes for the end game. When a user ends the game it will reload all the pages for the users. It will then notice the page has ended and will end accordingly.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,13 @@ class GamesController < ApplicationController
     redirect_to new_game_path
   end
 
+  def index
+    user_game_id = @current_user.select(:current_game).first.attributes.values[1]
+    if user_game_id != 0
+      id = Cargame.where(game_id: game_id).pluck(:id)
+      Cargame.delete(id)
+      flash[:notice] = "Your previous game with ID "+user_game_id+" was deleted!"
+    end
+  end
+
 end
